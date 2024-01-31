6d27fffab9d3828dc4897b51d18685339f92516d
imjesusc <iamjesus.ds@gmail.com>
2024-01-31 17:11:02 -0500

    refactor(projects/compare): Remove unused styles and select image feature.

--- projects/compare/README.md
+++ projects/compare/README.md
@@ -4,17 +4,12 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/f88d860e-7659-42b8-98aa-d5a538567881/deploy-status)](https://app.netlify.com/sites/compare-ui/deploys)
 
-![Compare](./src/public/mobile.jpg)
-
 ## Descripción
 
-Comparador de Imágenes Interactivo. Esta aplicación te permite comparar fácilmente imágenes y personalizar el fondo de pantalla según tus preferencias.
-
+Comparador de Imágenes Interactivo. Esta aplicación te permite comparar fácilmente imágenes.
 Características Principales:
 
 - Comparación de Imágenes: Explora la función de comparación de imágenes para analizar visualmente las diferencias y similitudes.
-
-- Personalización del Fondo de Pantalla: Cambia el fondo de pantalla de la aplicación según tus intereses y gustos personales.
 
 ## Referencias
 
--- projects/compare/src/main.js
+++ projects/compare/src/main.js
@@ -1,35 +1,3 @@
-const theme = document.querySelector('#theme')
-const range = document.getElementById('range')
-
 range.oninput = () => {
-  document.body.style.setProperty('--pos', range.value + '%')
+	document.body.style.setProperty('--pos', range.value + '%')
 }
-
-theme.oninput = () => {
-  document.body.style.setProperty('--base', theme.value)
-}
-
-const images = document.querySelectorAll('.section__image')
-const compareImages = document.querySelectorAll('.compare__image')
-
-const memory = {
-  previousIndexCompare: 0
-}
-
-const changeImage = () => {
-  images.forEach((image) => {
-    image.addEventListener('click', () => {
-      if (memory.previousIndexCompare === 0) {
-        compareImages[0].src = image.src
-        compareImages[0].setAttribute('alt', image.alt)
-        memory.previousIndexCompare = 1
-      } else {
-        image.style.opacity = 1
-        compareImages[1].src = image.src
-        memory.previousIndexCompare = 0
-      }
-    })
-  })
-}
-
-changeImage()
