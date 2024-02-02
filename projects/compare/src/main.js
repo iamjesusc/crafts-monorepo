4117f9c761169f7f8602d0a288e4e8e2bcad9e0e
imjesusc <iamjesus.ds@gmail.com>
2024-02-01 23:39:27 -0500

    refactor(projects/compare): Update code style with standard eslint

--- projects/compare/src/main.js
+++ projects/compare/src/main.js
@@ -1,3 +1,4 @@
+const range = document.getElementById('range')
 range.oninput = () => {
-	document.body.style.setProperty('--pos', range.value + '%')
+  document.body.style.setProperty('--pos', range.value + '%')
 }
